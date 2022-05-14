adfa50c99cab68edb10b8217ba61c04a349e202f
ldr123 <ldr12@163.com>
2022-05-14 11:44:28 +0800

    优化布局

--- Assets/Huatuo/Editor/ThirdPart/RippleEffect/RippleEffect.cs
+++ Assets/Huatuo/Editor/ThirdPart/RippleEffect/RippleEffect.cs
@@ -19,41 +19,32 @@ public class RippleEffect
         new Keyframe(0.85f, 0.52f, 0, 0),
         new Keyframe(0.99f, 0.50f, 0, 0)
     );
-    
+
     internal class Droplet
     {
-        Vector2 position;
-        float time;
-
-        public Droplet()
-        {
-            time = 1000;
-        }
+        public Vector4 MakeShaderParameter = new Vector4(0, 0, 1000, 0);
+        public float aspect = 1f;
 
         public void Reset()
         {
-            position = new Vector2(Random.value, Random.value);
-            time = 0;
+            MakeShaderParameter.x = Random.value;
+            MakeShaderParameter.y = Random.value * aspect;
+            MakeShaderParameter.z = 0;
         }
 
         public void Update()
         {
-            time += Time.deltaTime;
-        }
-
-        public Vector4 MakeShaderParameter(float aspect)
-        {
-            return new Vector4(position.x * aspect, position.y, time, 0);
+            MakeShaderParameter.z += Time.deltaTime;
         }
     }
 
     private float refractionStrength = 0.5f;
+    private float reflectionStrength = 0.6f;
     private Color reflectionColor = Color.gray;
-    private float reflectionStrength = 0.4f;
     private float waveSpeed = 1.25f;
-    private float dropInterval = 2.5f;
+    private float dropInterval = 2f;
     private float timer = 0;
-    private  int dropCount = 0;
+    private int dropCount = 0;
     private float aspect = 0;
 
     private Droplet[] droplets;
@@ -63,28 +54,40 @@ public class RippleEffect
 
     void UpdateShaderParameters()
     {
-        material.SetVector("_Drop1", droplets[0].MakeShaderParameter(aspect));
-        material.SetVector("_Drop2", droplets[1].MakeShaderParameter(aspect));
-        material.SetVector("_Drop3", droplets[2].MakeShaderParameter(aspect));
+        material.SetVector("_Drop1", droplets[0].MakeShaderParameter);
+        material.SetVector("_Drop2", droplets[1].MakeShaderParameter);
+        material.SetVector("_Drop3", droplets[2].MakeShaderParameter);
     }
 
     public void Init(Material _material, float _aspect)
     {
         aspect = _aspect;
         droplets = new Droplet[3];
-        droplets[0] = new Droplet();
-        droplets[1] = new Droplet();
-        droplets[2] = new Droplet();
+        droplets[0] = new Droplet
+        {
+            aspect = _aspect
+        };
+        droplets[1] = new Droplet
+        {
+            aspect = _aspect
+        };
+        droplets[2] = new Droplet
+        {
+            aspect = _aspect
+        };
 
-        gradTexture = new Texture2D(2048, 1, TextureFormat.Alpha8, false);
-        gradTexture.wrapMode = TextureWrapMode.Clamp;
-        gradTexture.filterMode = FilterMode.Bilinear;
+        gradTexture = new Texture2D(2048, 1, TextureFormat.Alpha8, false)
+        {
+            wrapMode = TextureWrapMode.Clamp,
+            filterMode = FilterMode.Bilinear
+        };
         for (var i = 0; i < gradTexture.width; i++)
         {
             var x = 1.0f / gradTexture.width * i;
             var a = waveform.Evaluate(x);
             gradTexture.SetPixel(i, 0, new Color(a, a, a, a));
         }
+
         gradTexture.Apply();
 
         material = _material;
@@ -93,15 +96,15 @@ public class RippleEffect
         material.SetColor("_Reflection", reflectionColor);
         material.SetVector("_Params1", new Vector4(aspect, 1, 1 / waveSpeed, 0));
         material.SetVector("_Params2", new Vector4(1, 1 / aspect, refractionStrength, reflectionStrength));
-        
+
         UpdateShaderParameters();
     }
 
     public void Destroy()
     {
-        GameObject.DestroyImmediate(gradTexture);
+        Object.DestroyImmediate(gradTexture);
     }
-    
+
     public void Update()
     {
         if (dropInterval > 0)
